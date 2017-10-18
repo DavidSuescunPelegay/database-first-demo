8145fac6ff04048f7e3785b5422dd7411d6370be
David Suescun Pelegay <dsuescun96@gmail.com>
2017-10-18 21:22:44 +0200

    Code upload

---
+++ DatabaseFirstDemo/DatabaseFirstDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseFirstDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instancia
+            var context = new DatabaseFirstDemoEntities();
+
+            //Nuevo Post
+            var post = new Posts()
+            {
+                Body = "Body",
+                DatePublished = DateTime.Now,
+                Title = "Title",
+                PostID = 1
+            };
+
+            //Añadirlo y guardar (esto no guarda en BBDD)
+            context.Posts.Add(post);
+
+            //Guardar en BBDD
+            context.SaveChanges();
+        }
+    }
+}
